0dcb9838616e4f4ebb802de080c4043aa7908239
kelstar159 <way.clarke@yahoo.com>
2016-06-12 21:36:45 +0000

    My first website

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+
+
 var appHelper = require("./apphelper.js");
 
 appHelper.runApp(function(app, db) {
-	
+
 
 	console.log("Travel application is running!");
 
-	var myName = "YOUR_NAME_HERE";
+	var myName = "Kelsee is the best";
 	app.get("/", function (req, resp) {
-		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
-		resp.end();
+		resp.render('index');
 	});
 
-})
+  app.get('/icecream', function(req, resp) {
+    resp.render('icecream', { flavor: 'chocolate' });
+  });
+
+  app.get('/blog', function(req, resp) {
+    db.findArray({}, function(results) {
+      console.log(results);
+      resp.render('blog', {results: results})
+    });
+  });
+});
